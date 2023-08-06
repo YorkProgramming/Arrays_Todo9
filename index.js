65f023158d6a39931a6c0ee54a1a184f4af165aa
Matthew York <myorkprogramming@gmail.com>
2023-08-06 03:02:10 -0700

    Median of Sorted Arrays

--- index.js
+++ index.js
@@ -36,3 +36,41 @@ return the average of the two middle values.
 Given ([1,5,9], [1,2,3,4,5]), return 3.5.
 
 Second: accept three arrays instead of two. */
+
+function medianOfSortedArrays(arr1, arr2){
+    var arr3 = [];
+    var median = 0;
+
+    for(var i = 0; i < arr1.length; i++){
+        arr3.push(arr1[i]);
+    }
+
+    for(var i = 0; i < arr2.length; i++){
+        arr3.push(arr2[i]);
+    }
+
+    arr3.sort(function(a, b){return a-b});
+
+    if(arr3.length % 2 == 0){
+        median = (arr3[arr3.length/2] + arr3[(arr3.length/2)-1])/2;
+    } else {
+        median = arr3[Math.floor(arr3.length/2)];
+    }
+
+    return median;
+}
+
+//Example:
+console.log(medianOfSortedArrays([1,5,9], [1,2,3,4,5,6]));
+
+//We create an empty array.
+//We create a median variable.
+//We loop through the first array and push the values into the empty array.
+//We loop through the second array and push the values into the empty array.
+//We sort the array.
+//We check if the array length is even.
+//If it is, we set the median to the average of the two middle values.
+//If it is not, we set the median to the middle value.
+//We return the median.
+
+//#########################################################################################################
