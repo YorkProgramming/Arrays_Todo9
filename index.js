586548ed65309e2969d9156545875720a9011737
Matthew York <myorkprogramming@gmail.com>
2023-08-08 23:38:00 -0700

    Time to English

--- index.js
+++ index.js
@@ -1,32 +1,4 @@
 //1.
-//Smarter Sum
-
-/* Use time-space tradeoff to accelerate average running time for iSigma(num) 
-that returns sum of integers from 1 to num. 
-
-Recall iSigma(1)=1, iSigma(2)=3, iSigma(3)=6, iSigma(4)=10.*/
-
-function iSigma(num){
-    var sum = 0;
-
-    for(var i = 0; i <= num; i++){
-        sum += i;
-    }
-
-    return sum;
-}
-
-//Example:
-console.log(iSigma(4));
-
-//We set a sum variable to 0.
-//We loop through the number.
-//We add the number to the sum.
-//We return the sum.
-
-//#########################################################################################################
-
-//2.
 //Median of Sorted Arrays
 
 /* Given two arrays that are sorted but not necessarily the same length, 
@@ -74,3 +46,17 @@ console.log(medianOfSortedArrays([1,5,9], [1,2,3,4,5,6]));
 //We return the median.
 
 //#########################################################################################################
+
+//2.
+//Time to English
+
+/* You are given an integer representing the number of minutes that have elapsed since midnight. 
+You should return a string  representing the current time, 
+in the traditional spoken convention. 
+Use numerals, except specifically the following words – 
+midnight, noon, past, til, half, quarter. 
+
+Examples: if given 30, return "half past midnight". 
+If given 75, return "quarter past 1 am". If given 710, 
+return "10 til noon". If given 1000, return "20 til 5 pm".
+*/
