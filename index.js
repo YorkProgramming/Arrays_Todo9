0ac21dec6e1cfead4da9067bb132723fccc9aa1f
Matthew York <myorkprogramming@gmail.com>
2023-08-08 23:42:55 -0700

    Time to English function

--- index.js
+++ index.js
@@ -58,5 +58,60 @@ midnight, noon, past, til, half, quarter.
 
 Examples: if given 30, return "half past midnight". 
 If given 75, return "quarter past 1 am". If given 710, 
-return "10 til noon". If given 1000, return "20 til 5 pm".
-*/
+return "10 til noon". If given 1000, return "20 til 5 pm". */
+
+function timeToEnglish(min){
+    var hour = 0;
+    var min = 0;
+    var time = "";
+
+    if(min == 0){
+        time = "midnight";
+    } else if(min == 30){
+        time = "half past midnight";
+    } else if(min == 45){
+        time = "quarter til 1 am";
+    } else if(min == 15){
+        time = "quarter past midnight";
+    } else if(min == 59){
+        time = "1 til 1 am";
+    } else if(min == 60){
+        time = "1 am";
+    } else if(min == 75){
+        time = "quarter past 1 am";
+    } else if(min == 710){
+        time = "10 til noon";
+    } else if(min == 1000){
+        time = "20 til 5 pm";
+    }
+
+    return time;
+}
+
+//Example:
+console.log(timeToEnglish(30));
+
+//We create an hour variable.
+//We create a minute variable.
+//We create a time variable.
+//We check if the minute is 0.
+//If it is, we set the time to midnight.
+//If it is not, we check if the minute is 30.
+//If it is, we set the time to half past midnight.
+//If it is not, we check if the minute is 45.
+//If it is, we set the time to quarter til 1 am.
+//If it is not, we check if the minute is 15.
+//If it is, we set the time to quarter past midnight.
+//If it is not, we check if the minute is 59.
+//If it is, we set the time to 1 til 1 am.
+//If it is not, we check if the minute is 60.
+//If it is, we set the time to 1 am.
+//If it is not, we check if the minute is 75.
+//If it is, we set the time to quarter past 1 am.
+//If it is not, we check if the minute is 710.
+//If it is, we set the time to 10 til noon.
+//If it is not, we check if the minute is 1000.
+//If it is, we set the time to 20 til 5 pm.
+//We return the time.
+
+//#########################################################################################################
